4907f4d396c6956b63baf974a3fe61966416a58a
Rahul <rahulrdx1308@gmail.com>
2022-10-19 23:22:36 +0530

    login register

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const connect = require("./configs/db");
+const cors = require("cors");
 
+const { register, login } = require("./controllers/auth.controller");
 
 const app = express();
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", register);
+
+app.post("/login", login);
 
 const port = process.env.PORT || 5000;
 app.listen(port, async () => {
